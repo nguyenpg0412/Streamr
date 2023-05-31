3b3fe7ce59b97d1da7a7a406f225e7419c8e900a
nguyenpg0412 <pgn12a5@gmail.com>
2023-05-31 09:31:21 +0700

    update & fix bugs

--- test/test.js
+++ test/test.js
@@ -1,11 +1,70 @@
-const str = '<a href="https://frens.streamr.network/mail/activation?token=6c11ad24ffde5bac2e0be87ff228894b">Found</a>';
 
-const regex = /<a href="(.*?)">.*?<\/a>/;
-const match = str.match(regex);
+const request = require('request');
 
-if (match && match[1]) {
-  const href = match[1];
-  console.log(href); // Output: https://frens.streamr.network/mail/activation?token=6c11ad24ffde5bac2e0be87ff228894b
-} else {
-  console.log('No link found.');
+// var options = {
+//   'method': 'GET',
+//   'url': 'https://api.internal.temp-mail.io/api/v3/email/a6v25xahxk@dishcatfish.com/messages',
+//   'headers': {
+//   }
+// };
+// request(options, function (error, response) {
+//   if (error) throw new Error(error);
+//   const htmlArr = JSON.parse(response.body)[0].body_text;
+//   const hrefValues = htmlArr.slice(htmlArr.indexOf('Verify your email ( ') + 19, htmlArr.indexOf(')\n\n('));
+//   console.log(hrefValues.trim());
+// });
+
+// async function generateMail() {
+//   let mailName = null;
+// const options = {
+//   'method': 'POST',
+//   'url': 'https://api.internal.temp-mail.io/api/v3/email/new',
+//   'headers': {
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify({
+//     "min_name_length": 10,
+//     "max_name_length": 10
+//   })
+
+// };
+// request(options, function (error, response) {
+//   if (error) throw new Error(error);
+//   console.log(parseObj(response.body));
+// });
+
+// }
+const parseObj = obj => { return JSON.parse(obj) };
+
+function generateMail() {
+  let mailName = null;
+  const options = {
+    'method': 'POST',
+    'url': 'https://api.internal.temp-mail.io/api/v3/email/new',
+    'headers': {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "min_name_length": 10,
+      "max_name_length": 10
+    })
+
+  };
+  return new Promise((resolve, reject) => {
+    request(options, (result, err) => {
+      if (result) resolve(parseObj(result.body));
+      reject(err);
+    })
+  })
+
+
+
 }
+
+(async () => {
+  try {
+    console.log(await generateMail())
+  } catch (error) {
+    console.log(error);
+  }
+})();
--- utils/generateInfo.js
+++ utils/generateInfo.js
@@ -1,58 +1,92 @@
-const random_name = require('node-random-name');
-const crypto = require('crypto');
-const { ethers, Signer } = require('ethers');
-const provider = new ethers.providers.JsonRpcProvider('https://bsc-dataseed.binance.org/');
-const mailJs = require('@cemalgnlts/mailjs');
+const random_name = require("node-random-name");
+const crypto = require("crypto");
+const { ethers } = require("ethers");
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://bsc-dataseed.binance.org/"
+);
+const request = require("request");
+
+const mailJs = require("@cemalgnlts/mailjs");
+const parseObj = (obj) => {
+  return JSON.parse(obj);
+};
+
+async function generateMail() {
+  let mailName = null;
+  const options = {
+    method: "POST",
+    url: "https://api.internal.temp-mail.io/api/v3/email/new",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      min_name_length: 10,
+      max_name_length: 10,
+    }),
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    mailName = parseObj(response.body);
+  });
+
+  return mailName;
+}
 
 async function generateUser() {
-    try {
-        const userName = random_name({ first: true }) + random_name({ last: true });
-        const mailObj = new mailJs();
-        const jsonObj = await mailObj.createOneAccount();
-        const mailName = await jsonObj.data.username;
-        const mailPass = await jsonObj.data.password;
-        if (userName, mailName, mailPass)
-            return { userName, mailName, mailPass, mailObj };
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const userName = random_name({ first: true }) + random_name({ last: true });
+    const mailObj = new mailJs();
+    const jsonObj = await mailObj.createOneAccount();
+    const mailName = await jsonObj.data.username;
+    const mailPass = await jsonObj.data.password;
+    if ((userName, mailName, mailPass))
+      return { userName, mailName, mailPass, mailObj };
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function generateWallet() {
-    const id = crypto.randomBytes(32).toString('hex');
-    const privateKey = "0x" + id;
-    try {
-        const signer = new ethers.Wallet(privateKey, provider);
-        const walletAddress = signer.address;
-        if (signer) return { privateKey, walletAddress };
-    } catch (error) {
-        console.log(error);
-    }
+  const id = crypto.randomBytes(32).toString("hex");
+  const privateKey = "0x" + id;
+  try {
+    const signer = new ethers.Wallet(privateKey, provider);
+    const walletAddress = signer.address;
+    if (signer) return { privateKey, walletAddress };
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function getMailContent(mailObj) {
-    for (let i = 0; i < 6; i++) {
-        const getMsg = await mailObj.getMessages();
-        const [msg] = await getMsg.data;
-        if (msg == undefined) {
-            console.log("Getting Message...");
-            await timer(1500);
-        } else {
-            console.log('Get Message Success!');
-            const msgIdd = await msg.id;
-            const getMsgById = await mailObj.getMessage(msgIdd);
-            const htmlArr = await getMsgById.data.html;
-            const hrefRegex = /href=(["'])(.*?)\1/g;
-            const hrefValues = htmlArr.join('').match(hrefRegex).map(match => match.slice(6, -1));
-            return hrefValues[1];
-        }
-        if (i == 5) {
-            console.log('danm, captcha is sucked');
-            return false;
-        }
+  for (let i = 0; i < 6; i++) {
+    const getMsg = await mailObj.getMessages();
+    const [msg] = await getMsg.data;
+    if (msg == undefined) {
+      console.log("Getting Message...");
+      await timer(1500);
+    } else {
+      console.log("Get Message Success!");
+      const msgIdd = await msg.id;
+      const getMsgById = await mailObj.getMessage(msgIdd);
+      const htmlArr = await getMsgById.data.html;
+      const hrefRegex = /href=(["'])(.*?)\1/g;
+      const hrefValues = htmlArr
+        .join("")
+        .match(hrefRegex)
+        .map((match) => match.slice(6, -1));
+      return hrefValues[1];
     }
+    if (i == 5) {
+      console.log("danm, captcha is sucked");
+      return false;
+    }
+  }
 }
-const timer = ms => new Promise(res => setTimeout(res, ms));
 
+// async function getMailContentV2() {
 
-module.exports = { generateUser, generateWallet, getMailContent };
+// }
+const timer = (ms) => new Promise((res) => setTimeout(res, ms));
+
+module.exports = { generateUser, generateWallet, getMailContent, generateMail };
